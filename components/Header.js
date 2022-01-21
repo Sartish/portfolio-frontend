4ed0fe94ba75d6671b073deb43c90303cb85fe4b
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-21 21:58:29 +0100

    checking alignment

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 
-import plant from "../assets/plant.png"
-import Image from "next/image"
+/* import plant from "../assets/plant.png"
+import Image from "next/image" */
 const Header = () => {
 
 
 	return (
 		<>
-			<header className="relative bg-yellow h-screen clip-path-slant-down-right flex justify-center items-center w-full">
+			<header className="relative h-screen clip-path-slant-down-right flex justify-center items-center w-full">
 				<div className="absolute top-40 h-2/5 flex justify-between flex-col p-2">
 					<h1 className="font-Rubik lg:text-xl text-base leading-7 tracking-widest">Hi! My name is</h1>
 					<div className="h-1/2 mt-40px">
@@ -18,7 +18,7 @@ const Header = () => {
 				</div>
 			</header>
 			<section className="absolute lg:bottom-10 bottom-0 right-0 lg:right-6">
-				<h1 className="m-0 text-center w-60 lg:w-4/5"><Image src={plant} /></h1>
+				{/* 	<h1 className="m-0 text-center w-60 lg:w-4/5"><Image src={plant} /></h1> */}
 			</section>
 		</>
 
