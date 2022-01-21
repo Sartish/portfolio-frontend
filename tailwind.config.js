a27992d31e0f4ea3f05afc08cc8b22dfad3d6bed
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-21 12:43:16 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-	future: {
-		removeDeprecatedGapUtilities: true,
-	},
-	purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
 	theme: {
-		extend: {
-			colors: {
-				'accent-1': '#333',
-			},
+		colors: {
+			'yellow': '#fdf5df'
+		},
+		fontFamily: {
+			'Rubik': ['Rubik', 'sans-serif'],
 		},
 	},
-	variants: {},
 	plugins: [],
 }
