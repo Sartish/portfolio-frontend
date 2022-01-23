53d735b547dc677e2780ed555502507b87c25192
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-23 14:03:21 +0100

    responsive

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 
 	return (
 		<>
-			<header className="relative h-screen clip-path-slant-down-right flex justify-center items-center w-full">
+			<header className="flex justify-center items-center border-2">
 				<div className="absolute top-40 h-2/5 flex justify-between flex-col p-2">
 					<h1 className="font-Rubik lg:text-xl text-base leading-7 tracking-widest">Hi! My name is</h1>
 					<div className="h-1/2 mt-40px">
@@ -17,7 +17,7 @@ const Header = () => {
 					<button className="mt-10">see my mork</button>
 				</div>
 			</header>
-			<section className="absolute lg:bottom-10 bottom-0 right-0 lg:right-6">
+			<section className="lg:bottom-10 bottom-0 right-0 lg:right-6">
 				{/* 	<h1 className="m-0 text-center w-60 lg:w-4/5"><Image src={plant} /></h1> */}
 			</section>
 		</>
