e54f5b574005be58bd12b86c86242065d3650ebf
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-23 19:35:58 +0100

    testing

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -9,15 +9,14 @@ const AboutMe = () => {
 			<section className="flex justify-center items-center flex-col mt-0 my-20">
 				<h1>About me</h1>
 				<div className="flex justify-center items-center w-2/3 flex-col lg:flex-row p-5">
-					<div className="p-10 w-60 lg:w-full">
-						<Image src={me}></Image>
+					<div className="p-10 w-60 lg:w-full photo">
+						<Image className="photo" src={me}></Image>
 					</div>
 					<p>
 						Hello there my friend! I am a frontend developer with a interest in
 						both design and content creation. I aim to combine my code skills &
 						passion for meaningful product & service ideas. To create
 						user-friendly digital platforms that are innovative and creative.
-
 					</p>
 				</div>
 			</section>
--- components/Header.js
+++ components/Header.js
@@ -7,16 +7,16 @@ const Header = () => {
 	return (
 		<>
 			<header className="w-full h-screen flex flex-col lg:flex-row justify-center items-center p-5 lg:p-40">
-				<div className="flex flex-col w-96 lg:w-full justify-between lg:justify-center items-center lg:items-start lg:mr-5 lg:mt-0 mt-10">
+				<div className="flex flex-col w-96 lg:w-full justify-between lg:justify-center items-center lg:items-start lg:mr-5 lg:mt-10 mt-10">
 					<h1 className="font-Rubik lg:text-xl text-lg leading-7 tracking-widest mb-5 bg-yellow p-2">Hi! My name is</h1>
 					<div className="flex clip-path-arrow-down lg:clip-path-arrow-right mt-10 mb-10 lg:mt-0 lg:mb-0 w-60 lg:w-full">
 						<Image src={mountain} />
 					</div>
 				</div>
 				<div className="flex lg:justify-around items-center flex-col mt-10 lg:w-full w-full">
-					<h2 className="font-Rubik lg:text-6xl text-2xl leading-10 tracking-widest p-2 mt-0 lg:mt-20 lg:p-3 lg:w-full lg:text-center">Sara Carlstein</h2>
-					<h2 className="font-Rubik lg:text-6xl text-2xl leading-10 tracking-widest lg:p-3 lg:w-full lg:text-center">&</h2>
-					<h2 className="font-Rubik lg:text-4xl text-2xl leading-10 tracking-widest text-center lg:p-3 lg:w-full">I like building things on the web</h2>
+					<h2 className="font-Rubik lg:text-6xl text-2xl leading-10 tracking-widest mt-0 lg:mt-20 lg:w-full lg:text-center"><span>Sara </span>Carlstein</h2>
+					<h2 className="font-Rubik lg:text-6xl text-2xl leading-10 tracking-widest lg:p-3 p-3 lg:w-full lg:text-center text-center">&</h2>
+					<h2 className="font-Rubik lg:text-4xl text-2xl leading-10 tracking-widest text-center lg:p-3 lg:w-full">I like <span className="bg-yellow p-5">building</span> things for the <span>web</span></h2>
 				</div>
 			</header>
 		</>
