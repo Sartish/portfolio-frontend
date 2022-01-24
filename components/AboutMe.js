3c1727e0529bfb007e07e56ac55b5f5ffac67a35
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-24 10:01:10 +0100

    repsonsiveness

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -6,10 +6,10 @@ const AboutMe = () => {
 
 	return (
 		<>
-			<section className="flex justify-center items-center flex-col mt-0 my-20">
+			<section className="flex justify-center items-center flex-col mt-40 lg:mt-10 my-20">
 				<h1>About me</h1>
-				<div className="flex justify-center items-center w-2/3 flex-col lg:flex-row p-5">
-					<div className="p-10 w-60 lg:w-full photo">
+				<div className="flex justify-center items-center w-2/3 flex-col lg:flex-row">
+					<div className="w-60 lg:w-full photo">
 						<Image className="photo" src={me}></Image>
 					</div>
 					<p>
@@ -17,6 +17,7 @@ const AboutMe = () => {
 						both design and content creation. I aim to combine my code skills &
 						passion for meaningful product & service ideas. To create
 						user-friendly digital platforms that are innovative and creative.
+						<p>What I also like: (animation)</p>
 					</p>
 				</div>
 			</section>
--- components/Header.js
+++ components/Header.js
@@ -6,17 +6,17 @@ const Header = () => {
 
 	return (
 		<>
-			<header className="w-full h-screen flex flex-col lg:flex-row justify-center items-center p-5 lg:p-40">
-				<div className="flex flex-col w-96 lg:w-full justify-between lg:justify-center items-center lg:items-start lg:mr-5 lg:mt-10 mt-10">
-					<h1 className="font-Rubik lg:text-xl text-lg leading-7 tracking-widest mb-5 bg-yellow p-2">Hi! My name is</h1>
-					<div className="flex clip-path-arrow-down lg:clip-path-arrow-right mt-10 mb-10 lg:mt-0 lg:mb-0 w-60 lg:w-full">
+			<header className="w-full h-screen flex flex-col lg:flex-row md:flex-row justify-center items-center p-5 lg:p-40 pt-60">
+				<div className="flex flex-col w-96 lg:w-full md:w-full justify-between lg:justify-center md:justify-center items-center lg:items-start md:items-start lg:mr-5 md:mr-5 lg:mt-10 md:mt-10 mt-10">
+					<div className="flex clip-path-arrow-down md:clip-path-arrow-right lg:clip-path-arrow-right mt-10 mb-10 lg:mt-0 md:mt-0 lg:mb-0 md:mb-0 w-96 lg:w-full md:w-full">
 						<Image src={mountain} />
 					</div>
 				</div>
-				<div className="flex lg:justify-around items-center flex-col mt-10 lg:w-full w-full">
-					<h2 className="font-Rubik lg:text-6xl text-2xl leading-10 tracking-widest mt-0 lg:mt-20 lg:w-full lg:text-center"><span>Sara </span>Carlstein</h2>
-					<h2 className="font-Rubik lg:text-6xl text-2xl leading-10 tracking-widest lg:p-3 p-3 lg:w-full lg:text-center text-center">&</h2>
-					<h2 className="font-Rubik lg:text-4xl text-2xl leading-10 tracking-widest text-center lg:p-3 lg:w-full">I like <span className="bg-yellow p-5">building</span> things for the <span>web</span></h2>
+				<div className="flex lg:justify-around md:justify-around items-center flex-col mt-10 lg:w-full md:w-full w-full">
+					{/* 	<h1 className="font-Rubik lg:text-xl text-lg leading-7 tracking-widest mb-5 bg-yellow p-2">Hi! I am</h1> */}
+					<h2 className="font-Rubik lg:text-6xl md:text-6xl text-4xl leading-normal mt-0 lg:w-full md:w-full lg:text-center md:text-center text-center"><span className="lg:text-4xl text-2xl">I'am </span>Sara Carlstein</h2>
+					<h2 className="font-Rubik lg:text-6xl md:text-5xl text-2xl leading-normal lg:p-2 p-3 lg:w-full md:w-full text-center">&</h2>
+					<h2 className="font-Rubik lg:text-4xl md:text-3xl text-2xl lg:leading-loose md:leading-normal text-center lg:w-full md:w-full">I like <span className="bg-yellow p-2">building</span> things for the <span>web</span></h2>
 				</div>
 			</header>
 		</>
