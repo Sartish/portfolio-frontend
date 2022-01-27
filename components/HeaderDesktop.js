eb4685c9c961809212b308bd4f92237273646d02
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-27 19:28:20 +0100

    new things

--- components/HeaderDesktop.js
+++ components/HeaderDesktop.js
@@ -8,12 +8,12 @@ const HeaderDesktop = () => {
 				<div className="flex justify-center items-center lg:w-[800px] md:w-[600px] w-[400px] p-5 z-0 pb-5">
 					<Image src={flight} />
 				</div>
-				<div className="flex justify-center items-center flex-col lg:min-w-fit z-10 p-5">
-					<h1 className="text-center font-Lora lg:text-[70px] md:text-[60px] text-[40px] tracking-wide lg:ml-[-200px] md:ml-[0px] lg:mb-[40px] mb-[20px] lowercase">Sara Carlstein</h1>
+				<div className="flex justify-center items-center flex-col lg:min-w-fit z-10">
+					<h1 className="text-center font-Lora lg:text-[70px] md:text-[60px] text-[40px] tracking-wide lg:ml-[-200px] md:ml-[0px] lg:mb-[40px] mb-[5px] lowercase">Sara Carlstein</h1>
 					<h1 className="text-center font-Poppins lg:ml-[-40px] md:ml-[0px] leading-12 tracking-wide text-[20px] md:text-[25px] lg:text-[30px] uppercase">Building things on the web</h1>
 				</div>
 			</div>
-			<h1 className="text-center font-Lora text-[14px] md:text-[18px] mt-10 lg:mt-[40px]">using my passion to help you pursue yours
+			<h1 className="text-center font-Lora text-[14px] md:text-[18px] mt-5 lg:mt-[40px]">using my passion to help you pursue yours
 				<h2 className="text-center uppercase font-Poppins font-semibold text-sm">learn more</h2>
 				<a className="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
 			</h1>
