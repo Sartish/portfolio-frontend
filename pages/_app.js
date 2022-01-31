adaae0990cee5a46bcde210ea7de0ddbc698e015
Sara Carlstein <sara.carlstein@capgemini.com>
2022-01-31 17:08:43 +0100

    Hack to fix scrollign issue

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
 	useEffect(() => {
 		// some browsers (like safari) may require a timeout to delay calling this
 		// function after a page has loaded; otherwise, it may not update the position
-		window.scrollTo(0, 0);
+		setTimeout(() => window.scrollTo(0, 0), 100)
 	}, [pathname]);
 
 	return <Component {...pageProps} />
