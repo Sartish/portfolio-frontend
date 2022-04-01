18260c4d1a3e48f7f7a49513977e6d9fa8d2a483
Sara Carlstein <carlstein.sara@gmail.com>
2022-04-01 11:05:11 +0200

    take away punchline

--- components/HeaderDesktop.js
+++ components/HeaderDesktop.js
@@ -1,48 +1,62 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import flight from "../assets/coversmall.png";
-import { Link, animateScroll as scroll } from "react-scroll";//Alternate for tag. In Nexjs we use link for ref
-import * as Scroll from 'react-scroll';
+import { Link, animateScroll as scroll } from "react-scroll"; //Alternate for tag. In Nexjs we use link for ref
+import * as Scroll from "react-scroll";
 
 const HeaderDesktop = () => {
+  return (
+    <header className="w-full h-[640px] md:h-[800px] lg:h-screen lg:screen flex justify-evenly lg:justify-start items-center flex-col lg:[mb-40]">
+      <div className="flex justify-center items-center flex-col lg:flex-row md:flex-col w-[90%]">
+        <div className="pl-10 pr-10 lg:p-0 flex justify-center items-center lg:w-[600px] md:w-[600px] w-[400px] z-0 lg:pt-10">
+          <Image src={flight} alt="picture for header" />
+        </div>
+        <div className="flex justify-center items-center flex-col lg:min-w-fit z-10">
+          <h1 className="text-center font-Lora lg:text-[90px] md:text-[60px] text-[40px] tracking-wide lg:ml-[-120px] md:ml-[0px] lg:mb-[40px] mb-[5px] md:mb-[20px] lowercase">
+            Sara Carlstein
+          </h1>
+          <h1 className="text-center font-Poppins lg:ml-[-40px] md:ml-[0px] leading-12 tracking-wide text-[20px] md:text-[20px] lg:text-[25px] uppercase">
+            Building things on the web
+          </h1>
+        </div>
+      </div>
+      <h1 className="text-center font-Lora text-[12px] md:text-[18px] mt-[-60px] lg:pt-[100px] md:mb-[30px]">
+        <h2 className="text-center uppercase font-Poppins font-semibold text-sm">
+          learn more
+        </h2>
+        <Link
+          to="third-desktop"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-200}
+          activeclassname="About"
+          className="hidden lg:block hover:opacity-50"
+        >
+          <a
+            className="ca3-scroll-down-link ca3-scroll-down-arrow"
+            data-ca3_iconfont="ETmodules"
+            data-ca3_icon=""
+          ></a>
+        </Link>
+        <Link
+          to="third-mobile"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-20}
+          activeclassname="About"
+          className="block lg:hidden hover:opacity-50"
+        >
+          <a
+            className="ca3-scroll-down-link ca3-scroll-down-arrow"
+            data-ca3_iconfont="ETmodules"
+            data-ca3_icon=""
+          ></a>
+        </Link>
+      </h1>
+    </header>
+  );
+};
 
-	return (
-		<header className="w-full h-[640px] md:h-[800px] lg:h-screen lg:screen flex justify-evenly lg:justify-start items-center flex-col lg:[mb-40]">
-			<div className="flex justify-center items-center flex-col lg:flex-row md:flex-col w-[90%]">
-				<div className="pl-10 pr-10 lg:p-0 flex justify-center items-center lg:w-[600px] md:w-[600px] w-[400px] z-0 lg:pt-10">
-					<Image src={flight} alt="picture for header" />
-				</div>
-				<div className="flex justify-center items-center flex-col lg:min-w-fit z-10">
-					<h1 className="text-center font-Lora lg:text-[90px] md:text-[60px] text-[40px] tracking-wide lg:ml-[-120px] md:ml-[0px] lg:mb-[40px] mb-[5px] md:mb-[20px] lowercase">Sara Carlstein</h1>
-					<h1 className="text-center font-Poppins lg:ml-[-40px] md:ml-[0px] leading-12 tracking-wide text-[20px] md:text-[20px] lg:text-[25px] uppercase">Building things on the web</h1>
-				</div>
-			</div>
-			<h1 className="text-center font-Lora text-[12px] md:text-[18px] mt-[-80px] lg:pt-[100px] md:mb-[30px]">using my passion to help you pursue yours
-				<h2 className="text-center uppercase font-Poppins font-semibold text-sm">learn more</h2>
-				<Link
-					to="third-desktop"
-					spy={true}
-					smooth={true}
-					duration={500}
-					offset={-200}
-					activeclassname="About"
-					className="hidden lg:block hover:opacity-50">
-					<a className="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
-				</Link>
-				<Link
-					to="third-mobile"
-					spy={true}
-					smooth={true}
-					duration={500}
-					offset={-20}
-					activeclassname="About"
-					className="block lg:hidden hover:opacity-50">
-					<a className="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
-				</Link>
-			</h1>
-		</header>
-	)
-
-}
-
-export default HeaderDesktop
+export default HeaderDesktop;
--- components/Skills.js
+++ components/Skills.js
@@ -2,149 +2,150 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import DevIcon, { iconList, RandomIcon } from "devicon-react-svg";
-import Head from 'next/head'
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-
+import Head from "next/head";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 console.log(`Here is a list of icon names used by this component: ${iconList}`);
 
 const Skills = () => {
-	useEffect(() => {
-		Aos.init({ duration: 1000 });
-	}, []);
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
-
-	return (
-		<div className="mt-20 lg:mt-20 lg:mb-20">
-			<SkillsContainer>
-				<HeaderContainer data-aos="fade-up">
-					<h1>Skills & Tools</h1>
-					<h2>What I use to create</h2>
-				</HeaderContainer>
-				<IconWrapper data-aos="fade-up" className="grid grid-cols-3 md:grid-cols-4 items-center justify-items-center gap-12">
-					<StyledIcon>
-						<i className="devicon-nextjs-original"></i>
-						<IconText>Nextjs</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-react-original"></i>
-						<IconText>React</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-css3-plain"></i>
-						<IconText>CSS3</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-figma-plain"></i>
-						<IconText>Figma</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-javascript-plain"></i>
-						<IconText>JS</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-mongodb-plain"></i>
-						<IconText>Mongo</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-github-original"></i>
-						<IconText>Github</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-html5-plain"></i>
-						<IconText>HTML5</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-git-plain"></i>
-						<IconText>Git</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-npm-original-wordmark"></i>
-						<IconText>NPM</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-express-original"></i>
-						<IconText>Express</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-nodejs-plain"></i>
-						<IconText>Nodejs</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-materialui-plain"></i>
-						<IconText>Material UI</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-visualstudio-plain"></i>
-						<IconText>Visual Studio</IconText>
-					</StyledIcon>
-					<StyledIcon>
-						<i className="devicon-sass-original"></i>
-						<IconText>SASS</IconText>
-					</StyledIcon>
-				</IconWrapper>
-				<HeaderTwo data-aos="fade-up">I also like...</HeaderTwo>
-				<More data-aos="fade-up" className="w-[90%] items-center justify-items-center p-20 grid grid-cols-2 md:grid-cols-4 content-center justify-center gap-10">
-					<p>sanity</p>
-					<p>shopify</p>
-					<p>UX/UI</p>
-					<p>design</p>
-					<p>Tailwind</p>
-					<p>creating content</p>
-					<p>design</p>
-					<p>content layout</p>
-				</More>
-			</SkillsContainer>
-		</div>
-	);
+  return (
+    <div className="mt-20 lg:mt-20 lg:mb-20">
+      <SkillsContainer>
+        <HeaderContainer data-aos="fade-up">
+          <h1>Skills & Tools</h1>
+          <h2>What I use to create</h2>
+        </HeaderContainer>
+        <IconWrapper
+          data-aos="fade-up"
+          className="grid grid-cols-3 md:grid-cols-4 items-center justify-items-center gap-12"
+        >
+          <StyledIcon>
+            <i className="devicon-nextjs-original"></i>
+            <IconText>Nextjs</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-react-original"></i>
+            <IconText>React</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-css3-plain"></i>
+            <IconText>CSS3</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-figma-plain"></i>
+            <IconText>Figma</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-javascript-plain"></i>
+            <IconText>JS</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-mongodb-plain"></i>
+            <IconText>Mongo</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-github-original"></i>
+            <IconText>Github</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-html5-plain"></i>
+            <IconText>HTML5</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-git-plain"></i>
+            <IconText>Git</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-npm-original-wordmark"></i>
+            <IconText>NPM</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-express-original"></i>
+            <IconText>Express</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-nodejs-plain"></i>
+            <IconText>Nodejs</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-materialui-plain"></i>
+            <IconText>Material UI</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-visualstudio-plain"></i>
+            <IconText>Visual Studio</IconText>
+          </StyledIcon>
+          <StyledIcon>
+            <i className="devicon-sass-original"></i>
+            <IconText>SASS</IconText>
+          </StyledIcon>
+        </IconWrapper>
+        <HeaderTwo data-aos="fade-up">I also like...</HeaderTwo>
+        <More
+          data-aos="fade-up"
+          className="w-[90%] items-center justify-items-center p-20 grid grid-cols-2 md:grid-cols-4 content-center justify-center gap-10"
+        >
+          <p>sanity</p>
+          <p>jira</p>
+          <p>UX/UI</p>
+          <p>design</p>
+          <p>tailwind</p>
+          <p>intelliJ</p>
+          <p>design</p>
+          <p>content layout</p>
+        </More>
+      </SkillsContainer>
+    </div>
+  );
 };
 
 export default Skills;
 
-
 const SkillsContainer = styled.div`
-	width: 100%;
-	display: flex; 
-	justify-content: center; 
-	flex-direction: column; 
-	align-items: center; 
-	margin-bottom: 30px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
 `;
 const HeaderContainer = styled.div`
-margin-top: 0;
- margin-bottom: 40px;
- text-align: center;
+  margin-top: 0;
+  margin-bottom: 40px;
+  text-align: center;
 `;
 const HeaderTwo = styled.h2`
-	font-size: 14px;
+  font-size: 14px;
   font-family: "Poppins", sans-serif;
   letter-spacing: 4px;
   line-height: 20px;
-	text-transform: uppercase;
-	font-weight: 600;
-	text-align: center;
-	margin-top: 25px;
+  text-transform: uppercase;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 25px;
 `;
 
 const IconWrapper = styled.div`
-	padding: 20px;
-	width: 80%;
+  padding: 20px;
+  width: 80%;
 `;
 
 const StyledIcon = styled.div`
-    fill: black;
-    width: 60px;
-		display: flex; 
-		flex-direction: column; 
-		text-align: center;
+  fill: black;
+  width: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 `;
 
 const IconText = styled.h2`
-   font-size: 12px;
-	 padding: 3px;
+  font-size: 12px;
+  padding: 3px;
 `;
 
-const More = styled.div`
-
-`;
+const More = styled.div``;
